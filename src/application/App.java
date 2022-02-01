a488c6dd533ac2da299b64f4f8bf2810ef41db54
earl-stephens <stephensearl@hotmail.com>
2022-01-31 23:54:04 -0700

    Demonstrate interface inheritance

--- src/application/App.java
+++ src/application/App.java
@@ -3,8 +3,11 @@ package application;
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//since Machine implements the interface, you can
+		// do the below
+		DefaultRunnable m1 = new Machine();
+		m1.run();
+		m1.displayDetails();
 	}
 
 }
